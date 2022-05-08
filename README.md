f5ba4d5771b5d60dc535c8c9c104977a830cb942
John Eisenmann <esperantanaso@gmail.com>
2022-05-07 22:39:34 -0400

    Add script to assemble volumes

--- README.md
+++ README.md
@@ -3,12 +3,13 @@
 
 This is a C implementation of [WheatSystem](http://www.ostracodfiles.com/wheatsystem/menu.html) for the WheatBox 55A1 computer. I hope that a [Tractor](https://github.com/ostracod/tractor) implementation will supersede this one eventually. However, I need to satisfy my craving to build a cute little computer, so the Tractor version will need to wait. The code in this repo is largely derived from my older [wheatsystem-c](https://github.com/ostracod/wheatsystem-c) repo.
 
-## Compilation
+## Usage
 
 This project has the following system-wide dependencies:
 
 * avr-gcc
 * gcc (to compile the emulator for Unix-like systems)
+* Node.js version 16 or later (to assemble example volumes)
 
 To compile the AVR firmware:
 
@@ -20,6 +21,18 @@ To compile the emulator:
 
 ```
 make unix
+```
+
+To assemble example volumes:
+
+1. Clone the [WheatBytecode assembler](https://github.com/ostracod/wheatbytecode-asm) in the parent directory of `wheatsystem-c55a1`.
+1. Follow the instructions in the README of `wheatbytecode-asm` to set up the assembler.
+1. Perform this command from within the `wheatsystem-c55a1` repo: `node ./assembleVolumes.js`
+
+To run the emulator:
+
+```
+./build/main_unix (volumeFilePath)
 ```
 
 
---
+++ assembleVolumes.js
@@ -0,0 +1,163 @@
+
+const fs = require("fs");
+const pathUtils = require("path");
+const childProcess = require("child_process");
+
+const volumesDirectoryPath = pathUtils.join(__dirname, "volumes");
+const assemblerDirectoryPath = pathUtils.join(__dirname, "../wheatbytecode-asm");
+const fileTypeSet = {
+    generic: 0,
+    bytecodeApp: 1,
+    systemApp: 2
+};
+
+let logIndentationLevel = 0;
+
+class AssemblerError extends Error {
+    
+    constructor(assemblerMessages) {
+        super();
+        this.assemblerMessages = assemblerMessages;
+    }
+}
+
+class WheatSystemFile {
+    
+    constructor(name, attributes, content) {
+        this.name = name;
+        this.attributes = attributes;
+        this.content = content;
+    }
+}
+
+class WheatSystemVolume {
+    
+    constructor() {
+        const buffer = Buffer.alloc(4);
+        buffer.writeUInt32LE(0, 0);
+        this.buffers = [buffer];
+        this.address = buffer.length;
+    }
+    
+    addFile(file) {
+        const lastBuffer = this.buffers[this.buffers.length - 1];
+        const offset = (this.buffers.length > 1) ? 6 : 0;
+        lastBuffer.writeUInt32LE(this.address, offset);
+        const header = Buffer.alloc(10);
+        header.writeUInt8(file.attributes, 0);
+        header.writeUInt8(file.name.length, 1);
+        header.writeUInt32LE(file.content.length, 2);
+        header.writeUInt32LE(0, 6);
+        const buffer = Buffer.concat([header, Buffer.from(file.name), file.content]);
+        this.buffers.push(buffer);
+        this.address += buffer.length;
+    }
+    
+    createBuffer() {
+        return Buffer.concat(this.buffers);
+    }
+}
+
+const getLogIndentation = () => {
+    let output = "";
+    for (let count = 0; count < logIndentationLevel; count++) {
+        output += "    ";
+    }
+    return output;
+};
+
+const logMessage = (message) => {
+    console.log(getLogIndentation() + message);
+};
+
+const logPush = (message) => {
+    logMessage(message);
+    logIndentationLevel += 1;
+};
+
+const logPop = (message) => {
+    logIndentationLevel -= 1;
+    logMessage(message);
+};
+
+const createFileAttributes = (fileConfig) => {
+    const fileType = fileTypeSet[fileConfig.type];
+    if (typeof fileType === "undefined") {
+        throw new Error("Invalid file type. Possible file types include: " + Object.keys(fileTypeSet).join(", "));
+    }
+    let output = fileType
+    if (fileConfig.hasAdminPerm) {
+        output |= 0x08;
+    }
+    if (fileConfig.isGuarded) {
+        output |= 0x04;
+    }
+    return output;
+};
+
+const assembleBytecodeFile = (sourcePath, destinationPath) => {
+    const sourceName = pathUtils.basename(sourcePath);
+    logPush(`Assembling file "${sourceName}"...`);
+    if (fs.existsSync(destinationPath)) {
+        fs.unlinkSync(destinationPath);
+    }
+    const assemblerMessages = childProcess.execFileSync("node", [
+        pathUtils.join(assemblerDirectoryPath, "/dist/assemble.js"),
+        sourcePath
+    ]).toString();
+    if (!fs.existsSync(destinationPath)) {
+        throw new AssemblerError(assemblerMessages);
+    }
+    logPop(`Finished assembling file "${sourceName}".`);
+};
+
+const assembleVolume = (volumeDirectoryPath) => {
+    const volume = new WheatSystemVolume();
+    const volumeConfigPath = pathUtils.join(volumeDirectoryPath, "volumeConfig.json");
+    const volumeConfig = JSON.parse(fs.readFileSync(volumeConfigPath, "utf8"));
+    volumeConfig.files.forEach((fileConfig) => {
+        const fileName = fileConfig.name;
+        const filePath = pathUtils.join(volumeDirectoryPath, fileName);
+        if (fileConfig.type === "bytecodeApp") {
+            const assemblyFilePath = filePath + ".wbasm";
+            if (fs.existsSync(assemblyFilePath)) {
+                assembleBytecodeFile(assemblyFilePath, filePath);
+            }
+        }
+        const fileAttributes = createFileAttributes(fileConfig);
+        const fileContent = fs.readFileSync(filePath);
+        const file = new WheatSystemFile(fileName, fileAttributes, fileContent);
+        volume.addFile(file);
+    });
+    const volumePath = volumeDirectoryPath + ".dat";
+    const volumeBuffer = volume.createBuffer();
+    fs.writeFileSync(volumePath, volumeBuffer);
+    return volumePath;
+};
+
+logPush("Assembling volumes...");
+
+try {
+    fs.readdirSync(volumesDirectoryPath).forEach((name) => {
+        const volumeDirectoryPath = pathUtils.join(volumesDirectoryPath, name);
+        if (fs.lstatSync(volumeDirectoryPath).isDirectory()) {
+            logPush(`Assembling volume "${name}"...`);
+            const volumePath = assembleVolume(volumeDirectoryPath);
+            logPop(`Finished assembling volume "${name}".`);
+            logMessage("Assembled volume path: " + volumePath);
+        }
+    });
+} catch (error) {
+    if (error instanceof AssemblerError) {
+        console.log("Assembler failed with the following output:");
+        console.log("===============================================");
+        console.log(error.assemblerMessages);
+        process.exit(1);
+    } else {
+        throw error;
+    }
+}
+
+logPop("Finished assembling volumes.");
+
+
